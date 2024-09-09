456e5278019aeec4471176b139ac936483e4b73a
opilane <opilane@HP-CZC2349HV1>
2024-09-09 13:08:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace MangMadu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Määratlege esimene sümbol, mis joonistab
+            int x1 = 1; // x koordinaat
+            int y1 = 3; // y koordinaat
+            char sym1 = '*'; // sümbol joonistada
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+
+        }
+
+        // Funktsioon joonistussümboli kindlal positsioonil
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
