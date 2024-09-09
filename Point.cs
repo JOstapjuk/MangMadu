8c55ca9031da85a6a3133c70c7851f788fae5a96
opilane <opilane@HP-CZC2349HV1>
2024-09-09 13:30:53 +0300

    video 2 - lõi Pointi klassi ja kasutas seda

---
+++ Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangMadu
+{
+    internal class Point
+    {
+        // Kuulutame välja klassimuutujad
+        public int x;
+        public int y;
+        public char sym;
+
+        // Funktsioon joonistussümboli kindlal positsioonil
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,31 +1,23 @@
 ﻿using System;
+using System.Runtime.Serialization;
 namespace MangMadu
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Määratlege esimene sümbol, mis joonistab
-            int x1 = 1; // x koordinaat
-            int y1 = 3; // y koordinaat
-            char sym1 = '*'; // sümbol joonistada
-            Draw(x1, y1, sym1);
+            //loome klassis uue obekti
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
-
-            
-
-        }
-
-        // Funktsioon joonistussümboli kindlal positsioonil
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
         }
     }
 }
